e2a9cd081a3fa75f84aead32a1a73e5eee1fda97
Kalim Qureshi <17159@aegonlife.com>
2019-04-12 19:06:42 +0530

    new files

--- src/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-console */
-const app = require('./app');
-const handleListen = require('./handleListen');
-
-const PORT = 3000;
-app.listen(PORT, handleListen(console.log, PORT));
